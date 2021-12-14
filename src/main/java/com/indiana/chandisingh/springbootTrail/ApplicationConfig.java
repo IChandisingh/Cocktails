fbeb92fdb07698a139d5a0a939b609ad42932195
Indiana Chandisingh <ichandisingh@softwareinstitute.com>
2021-12-14 11:22:29 +0000

    test webhook

--- src/main/java/com/indiana/chandisingh/springbootTrail/ApplicationConfig.java
+++ src/main/java/com/indiana/chandisingh/springbootTrail/ApplicationConfig.java
@@ -1,23 +1,23 @@
-
 package com.indiana.chandisingh.springbootTrail;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.*;
 import com.google.gson.Gson;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-import java.util.Base64;
+//import javax.sql.DataSource;
+//import java.util.Base64;
 
 @Configuration
 public class ApplicationConfig {
 
 
-    //private Gson gson =new Gson();
+    private Gson gson =new Gson();
 
     /*
 
@@ -35,11 +35,7 @@ public class ApplicationConfig {
 
      */
 
-// Use this code snippet in your app.
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/java-dg-samples.html#prerequisites
-
-    public static void getSecret() {
+    private AwsSecrets getSecret() {
 
         String secretName = "arn:aws:secretsmanager:us-east-2:593831182595:secret:DB-oadAey";
         String region = "us-east-2";
@@ -47,6 +43,7 @@ public class ApplicationConfig {
         // Create a Secrets Manager client
         AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
                 .withRegion(region)
+                //.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("","")))
                 .build();
 
         // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
@@ -60,38 +57,23 @@ public class ApplicationConfig {
 
         try {
             getSecretValueResult = client.getSecretValue(getSecretValueRequest);
-        } catch (DecryptionFailureException e) {
-            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (InternalServiceErrorException e) {
-            // An error occurred on the server side.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (InvalidParameterException e) {
-            // You provided an invalid value for a parameter.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (InvalidRequestException e) {
-            // You provided a parameter value that is not valid for the current state of the resource.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (ResourceNotFoundException e) {
-            // We can't find the resource that you asked for.
-            // Deal with the exception here, and/or rethrow at your discretion.
+        } catch (Exception e) {
             throw e;
         }
 
-        // Decrypts secret using the associated KMS CMK.
-        // Depending on whether the secret is a string or binary, one of these fields will be populated.
         if (getSecretValueResult.getSecretString() != null) {
             secret = getSecretValueResult.getSecretString();
+            return gson.fromJson(secret,AwsSecrets.class);
         }
+        return null;
+        /*
         else {
             decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
         }
+         */
 
         // Your code goes here.
+
     }
 
 }
